b0e797305eed3d7747d807ecc19b9b8ec7f91fd3
epixieme <kirstie_hayes@hotmail.com>
2020-03-11 13:40:10 +0000

    Updates

---
+++ precache-manifest.6d1df5453e283ba298c4e5e6a1575b61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef61ca6582cfc909861ab61917c0218",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "adcc898a964911ca3889",
+    "url": "/myportfolio/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4d51be9d401eaf68833c",
+    "url": "/myportfolio/static/css/main.85134142.chunk.css"
+  },
+  {
+    "revision": "adcc898a964911ca3889",
+    "url": "/myportfolio/static/js/2.9f578cac.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myportfolio/static/js/2.9f578cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d51be9d401eaf68833c",
+    "url": "/myportfolio/static/js/main.2fd87e00.chunk.js"
+  },
+  {
+    "revision": "c91d6afdd95550065181",
+    "url": "/myportfolio/static/js/runtime-main.c21d50fe.js"
+  },
+  {
+    "revision": "e9f69585ae48cb7499b14783f924034a",
+    "url": "/myportfolio/static/media/logo.e9f69585.svg"
+  },
+  {
+    "revision": "cbe7be7f6d7c7846ec4652acfce685ed",
+    "url": "/myportfolio/static/media/robofriends.cbe7be7f.svg"
+  },
+  {
+    "revision": "55b34aa731bde461e96942ee9f40714b",
+    "url": "/myportfolio/static/media/shoreditch.55b34aa7.png"
+  }
+]);
